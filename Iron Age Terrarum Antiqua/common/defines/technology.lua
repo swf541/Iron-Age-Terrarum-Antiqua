fcce5d6ec03d94c7fdd1ff5f3b180ab4e5b5c34a
YourGithubAccountName <rickvisser94@hotmail.com>
2017-09-16 19:54:42 +0200

    Fixed technology across the board.

---
+++ Iron Age Terrarum Antiqua/common/defines/technology.lua
@@ -0,0 +1,4 @@
+-- Technology
+NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 417	-- Set this to your earliest starting year
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 400
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_8 = 1500
